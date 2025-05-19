99d2d52b631d2db1d6525e58571552ada7295f1e
Ígor José Rodrigues <igorjr2003@gmail.com>
2025-05-19 15:21:27 -0300

    ✨ Create User controller

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from 'src/modules/common/common.module';
 
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [CommonModule],
   providers: [UserService, UserRepository],
   exports: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
