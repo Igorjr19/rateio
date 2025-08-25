182fa661bd2bbd896e108631f88009c7d832d1ca
Ígor José Rodrigues <igorjr2003@gmail.com>
2025-08-25 08:02:59 -0300

    ✨ Update health check endpoint and service method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/health-check')
+  @Public()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.healthCheck();
   }
 }
